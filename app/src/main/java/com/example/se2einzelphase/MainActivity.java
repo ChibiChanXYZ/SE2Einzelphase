e363e5e1160401127632075e21eaef63a4889301
lisa <lisa.gassinger@gmx.at>
2021-03-13 21:56:36 +0100

    Create new thread for serverconnection and give permission for internet

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,8 +25,53 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView response = findViewById(R.id.MessageServer);
-                response.setText("Hier steht später die Antwort vom Server");
+
+                findViewById(R.id.MatrNr);
+
+
+
+                ThreadForButton p = new ThreadForButton(response);
+                p.start();
+
+                try {
+                    p.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
+
+
+
+    }
+}
+
+
+ class ThreadForButton extends Thread {
+    TextView response;
+
+    ThreadForButton(TextView response){
+        this.response= response;
+    }
+
+    public void run(){
+        try {
+
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+            response.setText("Hier steht später die Antwort vom Server");
+
+
+
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
